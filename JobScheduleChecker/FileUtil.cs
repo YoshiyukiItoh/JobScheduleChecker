137a282e80da0be91317f7ffd300d16aa7b96989
Yoshiyuki Itoh <swallows.vct1a19@gmail.com>
2016-03-27 03:19:32 +0900

    fileutil処理追加

--- JobScheduleChecker/FileUtil.cs
+++ JobScheduleChecker/FileUtil.cs
@@ -10,12 +10,31 @@ namespace JobScheduleChecker
 {
     class FileUtil : Base
     {
-        public FileUtil() { }
+        public FileUtil()
+        {
+            Init();
+        }
 
         private void Init()
         {
-            string[] files =getFiles(Common.monitor_path);
+            string[] files = getFiles(Common.monitor_path);
+            foreach (string file in files)
+            {
+                divideProcess(file);
+            }
+        }
 
+        public void divideProcess(string file)
+        {
+
+            if (file.IndexOf(Common.target_file) >= 0)
+            {
+                UploadFile(file);
+            }
+            else
+            {
+                DeleteFile(file);
+            }
         }
 
         private string[] getFiles(string path)
@@ -23,23 +42,38 @@ namespace JobScheduleChecker
             return Directory.GetFiles(path);
         }
 
-        private void MoveFile(string srcPath, string destPath)
+        private void DeleteFile(string path)
         {
-            logger.Info(String.Format("Move {0} -> {1}",srcPath,destPath));
-            File.Move(srcPath,destPath);
+            logger.Info(String.Format("Delete : {0}", path));
+            File.Delete(path);
         }
 
-        public void DecryptFile(string passphrase, string filename)
+        private void MoveFile(string srcPath, string destPath)
         {
-            string arg = String.Format("-d -k \"{0}\" {1}", passphrase,filename);
-            logger.Info(String.Format("Decrypt File : {0}", filename));
+            logger.Info(String.Format("Move : {0} -> {1}", srcPath, destPath));
+            File.Move(srcPath, destPath);
+        }
+
+        private void DecryptFile(string passphrase, string filename)
+        {
+            string arg = String.Format("-d -k \"{0}\" {1}", passphrase, filename);
+            logger.Info(String.Format("Decrypt Target File : {0}", filename));
             ExecCmd("Axcrypt", arg);
         }
 
         private void ExecCmd(string cmd, string arg)
         {
-            logger.Info(String.Format("Exec command : {0} {1}",cmd,arg));
+            logger.Info(String.Format("Exec command : {0} {1}", cmd, arg));
             Process.Start(cmd, arg);
+        }
+
+        private void UploadFile(string filePath)
+        {
+            string filename = Path.GetFileName(filePath);
+            string destFilePath = Path.Combine(Common.dest_path, filename);
+
+            MoveFile(filePath, destFilePath);
+            DecryptFile(Common.passphrase, destFilePath);
         }
     }
 }
