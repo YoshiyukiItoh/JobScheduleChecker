281e6df16ae10aeb063a0929ba52ef590598ef75
Yoshiyuki Itoh <swallows.vct1a19@gmail.com>
2016-03-26 22:24:43 +0900

    クラス追加

---
+++ JobScheduleChecker/Settings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobScheduleChecker
+{
+    class Settings
+    {
+        private string _passphrase;
+
+        public string Passphrase
+        {
+            get { return _passphrase; }
+            set { _passphrase = value; }
+        }
+    }
+}
