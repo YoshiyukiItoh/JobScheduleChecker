83286b3897e0fd49dc4d794e60e0150efbf9acb0
Yoshiyuki Itoh <swallows.vct1a19@gmail.com>
2016-03-26 21:42:35 +0900

    util処理追加

--- JobScheduleChecker/FileUtil.cs
+++ JobScheduleChecker/FileUtil.cs
@@ -3,10 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
 
 namespace JobScheduleChecker
 {
     class FileUtil
     {
+        public FileUtil() { }
+
+        private void Init()
+        {
+            string[] files =getFiles(Common.monitor_path);
+
+        }
+
+        private string[] getFiles(string path)
+        {
+            return Directory.GetFiles(path);
+        }
+
+        private void MoveFile(string srcPath, string destPath)
+        {
+            File.Move(srcPath,destPath);
+        }
+
+        public void DecryptFile(string filename)
+        {
+            string arg = String.Format("-d -k \"55555\" {0}", filename);
+            ExecCmd("Axcrypt", arg);
+        }
+
+        private void ExecCmd(string cmd, string arg)
+        {
+            Process.Start(cmd, arg);
+        }
     }
 }
