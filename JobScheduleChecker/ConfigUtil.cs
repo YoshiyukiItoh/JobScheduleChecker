62b712afc0465f7f6e715f41676da51b83d8468c
Yoshiyuki Itoh <swallows.vct1a19@gmail.com>
2016-03-27 02:23:20 +0900

    設定ファイル入出力クラス追加

---
+++ JobScheduleChecker/ConfigUtil.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+using System.IO;
+using System.Xml.Serialization;
+using System.Windows.Forms;
+
+namespace JobScheduleChecker
+{
+    class ConfigUtil : Base
+    {
+        private Settings appSettings;
+        private XmlSerializer serializer;
+
+        public ConfigUtil()
+        {
+            appSettings = new Settings();
+            serializer = new XmlSerializer(typeof(Settings));
+            Init();
+        }
+
+        private void Init()
+        {
+            if(File.Exists(Common.config_filePath))
+            {
+                ReadConfig();
+            }
+            else
+            {
+                CreateConfig();
+                MessageBox.Show("create conig\r\nplwase edit the config.");
+                logger.Info("create conig file. plwase edit the config file.");
+                Application.Exit();
+            }
+        }
+
+        private void CreateConfig()
+        {
+            StreamWriter sw 
+                = new StreamWriter(Common.config_filePath, false, new UTF8Encoding(false));
+            serializer.Serialize(sw, appSettings);
+            sw.Close();
+        }
+
+        private void ReadConfig()
+        {
+            StreamReader sr 
+                = new StreamReader(Common.config_filePath, new UTF8Encoding(false));
+            appSettings = (Settings)serializer.Deserialize(sr);
+            sr.Close();
+        }
+
+
+    }
+}
